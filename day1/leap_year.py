bd1229fcf0d019ac130ead79d44012fda6a0cd2d
Divyakn-shaiva <divyakn004@gmail.com>
2025-08-17 22:36:51 +0530

    updated a new file leap_year.py to day1

---
+++ day1/leap_year.py
@@ -0,0 +1,10 @@
+def is_leap_year (year) :
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+year = int(input("Enter the valid year:  " ))
+if is_leap_year(year):
+    print(year , "is leap year")
+else :
+    print(year , "is not leap year")
