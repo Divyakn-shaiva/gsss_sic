fceb2fdaa2135934bc5d0f93e4499b820402e23f
Divyakn-shaiva <divyakn004@gmail.com>
2025-09-03 01:04:34 +0530

    modified

--- day3/x_inside_hollow.py
+++ day3/x_inside_hollow.py
@@ -2,7 +2,7 @@ def x_inside_hollow_square(number_of_lines):
     for i in range(number_of_lines):
         for j in range(number_of_lines):
             if i == j or j == number_of_lines - i - 1 or i == 0 or i == number_of_lines - 1 or j == 0 or j == number_of_lines - 1 :
-                print(" * " , end = " ")
+                print("*" , end = " ")
             else:
                 print(" ",end = " ")
         print()
